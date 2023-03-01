49abdf6b9dbe0d06988bd551cce47611157e97be
E-Nagy <90033972+E-Nagy@users.noreply.github.com>
2023-03-01 13:49:16 +0100

    Übungen 2-1 bis 2-4

---
+++ M151/Uebung2-3/Bestellungen.php
@@ -0,0 +1,47 @@
+<?php
+$dbType = "mysql";
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "northwind";
+
+try {
+    $conn = new PDO("$dbType:host=$servername;dbname=$database", $username, $password);
+
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+} catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+?>
+<table>
+     <tr>
+         <th>Kundenname    </th> <!-- ship_name -->
+         <th>Addresse      </th>
+         <th>Stadt         </th>
+         <th>State/Province</th>
+     </tr>
+
+<?php
+$sql = "SELECT * FROM orders where customer_id = :id ";
+$order = $_GET['id'];
+$statement = $conn->prepare($sql);
+$statement->execute([
+    ':id' => $order
+]);
+
+
+while ($row = $statement -> fetch()) {
+    ?>
+    <tr>
+        <td> <?php echo $row['ship_name']?> </td>
+        <td> <?php echo $row['ship_address']?> </td>
+        <td> <?php echo $row['ship_city']?> </td>
+        <td> <?php echo $row['ship_state_province']?> </td>
+
+    </tr>
+    <?php
+}
+?>
+
+</table>
